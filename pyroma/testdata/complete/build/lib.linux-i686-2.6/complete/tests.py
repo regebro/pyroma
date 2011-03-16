724e85e45ddaef4d7caa7026cf328c6ad1dad5b8
Lennart Regebro <regebro@gmail.com>
2011-03-16 16:11:50 -0400

    Made a ProxyMock for xmlrpclib so we don't have to hit PyPI during testing.

--- pyroma/testdata/complete/build/lib.linux-i686-2.6/complete/tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-
-class PackageDataTest(unittest.TestCase):
-    
-    def test_test(self):
-        pass
-    
--- pyroma/tests.py
+++ pyroma/tests.py
@@ -1,5 +1,7 @@
 import unittest
 import os
+import xmlrpclib
+
 from pyroma import projectdata, distributiondata, pypidata
 from pyroma.ratings import rate
 from pkg_resources import resource_filename, resource_string
@@ -34,6 +36,57 @@ COMPLETE = {'name': 'complete',
                             #),
             }
 
+
+_RECORD = True # Set this to true, and it will use the real 
+
+real_server_proxy = xmlrpclib.ServerProxy
+    
+class ProxyStub(object):
+    def __init__(self, dataname, real_class, developmode):        
+        filename = resource_filename(
+            __name__, os.path.join('testdata', 'xmlrpcdata', dataname))
+        data = {}
+        exec(open(filename, 'rt').read(), None, data)
+        self.args = data['args']
+        self.kw = data['kw']
+        self._data = data['data']
+
+        if developmode:
+            self._real = real_class(*self.args, **self.kw)
+        else:
+            self._real = None
+        
+    def __call__(self, *args, **kw):
+        assert args == self.args
+        assert kw == self.kw
+        return self
+            
+    def _make_proxy(self, name):
+        def _proxy_method(*args, **kw):
+            return self._data[name][args]
+        
+        return _proxy_method
+            
+    def _make_unknown_proxy(self, name):
+        def _proxy_method(*args, **kw):
+            if self._real is None:
+                raise AttributeError('ProxyStub unkown method ' + name)
+            print
+            print "== ProxyStub unknown method =="
+            print name, ':', args, kw
+            result = getattr(self._real, name)(*args, **kw)
+            print "Result :"
+            print result
+            return result
+            
+        return _proxy_method
+
+    def __getattr__(self, attr):
+        if attr in self._data:
+            return self._make_proxy(attr)
+        return self._make_unknown_proxy(attr)
+    
+        
 class RatingsTest(unittest.TestCase):
     
     def test_complete(self):
@@ -66,14 +119,20 @@ class RatingsTest(unittest.TestCase):
         ]))
 
     def test_distribute(self):
-        # XXX: Mock PyPI
-        data = pypidata.get_data('distribute')
-        rating = rate(data)
+        try:
+            xmlrpclib.ServerProxy = ProxyStub('distributedata.py',
+                                              xmlrpclib.ServerProxy,
+                                              False)
+            
+            data = pypidata.get_data('distribute')
+            rating = rate(data)
+            
+            self.assertEqual(rating, (9, [
+                'The classifiers should specify what minor versions of Python you support as well as what major version',
+            ]))
+        finally:
+            xmlrpclib.ServerProxy = real_server_proxy
         
-        self.assertEqual(rating, (9, [
-            'The classifiers should specify what minor versions of Python you support as well as what major version',
-        ]))
-
 class ProjectDataTest(unittest.TestCase):
     
     def test_complete(self):
