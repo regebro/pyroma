7821024f0025a490f08aef8caf7addecf8776751
Lennart Regebro <regebro@users.noreply.github.com>
2013-10-18 21:27:29 +0200

    Using optparse. (because I support Python 2.6)

--- pyroma/__init__.py
+++ pyroma/__init__.py
@@ -1,5 +1,6 @@
-import sys
 import os
+import sys
+from optparse import OptionParser
 from pyroma import projectdata, distributiondata, pypidata, ratings
 
 import logging
@@ -18,22 +19,58 @@ def zester(data):
                 sys.exit(1)
 
 
-def run(argument):
+def main():
+    usage = "usage: %prog [-a|-d|-f|-p] <project directory|distribution file|pypi package name>"
+    parser = OptionParser(usage)
+    parser.add_option("-a", "--auto", dest="auto", default=False,
+                      action='store_true', help="Select mode automatically (default)",)
+    parser.add_option("-d", "--directory", dest="directory",
+                      action='store_true', default=False, 
+                      help="Run pyroma on a module in a project directory",)
+    parser.add_option("-f", "--file", dest="file", action='store_true',
+                      default=False, help="Run pyroma on a distribution file",)
+    parser.add_option("-p", "--pypi", dest="pypi", action='store_true',
+                      default=False, help="Run pyroma on a package on PyPI",)
+    
+    (options, args) = parser.parse_args()    
+
+    if len(args) < 1:
+        parser.print_help()
+        sys.exit(1)
+
+    modes = (options.auto, options.directory, options.file, options.pypi)
+    if sum([1 if x else 0 for x in modes]) > 1:
+        print("You can only select one of the options -a, -d, -f and -p")
+        sys.exit(1)
+
+    argument = args[0]        
+    
+    if not any(modes) or options.auto:
+        if os.path.isdir(argument):
+            mode = 'directory'
+        elif os.path.isfile(argument):
+            mode = 'file'
+    elif options.directory:
+        mode = 'directory'
+    elif options.file:
+        mode = 'file'
+    else:
+        mode = 'pypi'
+        
     logging.info('-'*30)
     logging.info('Checking ' + argument)
-    
-    if os.path.isdir(argument):
+            
+    if mode == 'directory':   
         data = projectdata.get_data(os.path.abspath(argument))
         logging.info('Found ' + data.get('name', 'nothing'))
-    
-    elif os.path.isfile(argument):
+    elif mode == 'file':
         data = distributiondata.get_data(os.path.abspath(argument))
         logging.info('Found ' + data.get('name', 'nothing'))
-        
     else:
         # It's probably a package name
         data = pypidata.get_data(argument)
         logging.info('Found ' + data.get('name', 'nothing'))
+        
     rating = ratings.rate(data)
         
     logging.info('-'*30)
@@ -48,11 +85,3 @@ def run(argument):
     logging.info(ratings.LEVELS[rating[0]])
     logging.info('-'*30)
     return rating[0]
-
-
-def main():
-    if len(sys.argv) < 2:
-        logging.info("Usage: pyroma <project directory|file|project name>")
-        sys.exit(1)
-
-    run(sys.argv[1])
