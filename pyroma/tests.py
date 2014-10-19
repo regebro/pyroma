905314a0c3d5ac0fd634acbc81889e3f94f4d313
Lennart Regebro <regebro@users.noreply.github.com>
2014-10-19 22:09:42 +0200

    Fixed the spelling errors in the tests as well.

--- pyroma/tests.py
+++ pyroma/tests.py
@@ -10,7 +10,7 @@ try:
 except ImportError:
     import xmlrpclib
     import urllib
-    
+
 from pyroma import projectdata, distributiondata, pypidata, zester
 from pyroma.ratings import rate
 from pkg_resources import resource_filename, resource_string
@@ -56,7 +56,7 @@ class FakeResponse(object):
             # 2.5 and lower doesn't have the code attribute.
             # The test should fail on Python 2.5.
             self.code = responsecode
-            
+
     def read(self):
         return open(self.filename, 'rb').read()
 
@@ -76,19 +76,19 @@ def urlopenstub(url):
         datafile = resource_filename(
             __name__, os.path.join('testdata', 'xmlrpcdata', filename+'.html'))
         return FakeResponse(200, datafile)
-        
-        
+
+
     if url.startswith('http://pypi.python.org/packages'):
         filename = [x for x in url.split('/') if x][-1]
         # Faking PyPI file downloads
         datafile = resource_filename(
             __name__, os.path.join('testdata', 'distributions', filename))
         return FakeResponse(200, datafile)
-    
+
     raise ValueError("Don't know how to stub " + url)
 
 class ProxyStub(object):
-    def __init__(self, dataname, real_class, developmode):        
+    def __init__(self, dataname, real_class, developmode):
         filename = resource_filename(
             __name__, os.path.join('testdata', 'xmlrpcdata', dataname))
         data = {}
@@ -101,18 +101,18 @@ class ProxyStub(object):
             self._real = real_class(*self.args, **self.kw)
         else:
             self._real = None
-        
+
     def __call__(self, *args, **kw):
         assert args == self.args
         assert kw == self.kw
         return self
-            
+
     def _make_proxy(self, name):
         def _proxy_method(*args, **kw):
             return self._data[name][args]
-        
+
         return _proxy_method
-            
+
     def _make_unknown_proxy(self, name):
         def _proxy_method(*args, **kw):
             if self._real is None:
@@ -124,23 +124,23 @@ class ProxyStub(object):
             print("Result :")
             print(result)
             return result
-            
+
         return _proxy_method
 
     def __getattr__(self, attr):
         if attr in self._data:
             return self._make_proxy(attr)
         return self._make_unknown_proxy(attr)
-    
-        
+
+
 class RatingsTest(unittest.TestCase):
-    
+
     def test_complete(self):
         directory = resource_filename(
             __name__, os.path.join('testdata', 'complete'))
         data = projectdata.get_data(directory)
         rating = rate(data)
-        
+
         # Should have a perfect score
         self.assertEqual(rating, (10, []))
 
@@ -149,19 +149,19 @@ class RatingsTest(unittest.TestCase):
             __name__, os.path.join('testdata', 'minimal'))
         data = projectdata.get_data(directory)
         rating = rate(data)
-        
+
         self.assertEqual(rating, (2, [
-            'The packages version number does not comply with PEP-386.', 
-            'The packages description should be longer than 10 characters.', 
-            'The packages long_description is quite short.', 
-            'Your package does not have classifiers data.', 
-            'You should specify what Python versions you support.', 
-            'Your package does not have keywords data.', 
-            'Your package does not have author data.', 
-            'Your package does not have author_email data.', 
-            'Your package does not have url data.', 
-            'Your package does not have license data.', 
-            'You are using Setuptools or Distribute but do not specify if this package is zip_safe or not. You should specify it, as it defaults to True, which you probably do not want.',
+            "The package's version number does not comply with PEP-386.",
+            "The package's description should be longer than 10 characters.",
+            "The package's long_description is quite short.",
+            "Your package does not have classifiers data.",
+            "You should specify what Python versions you support.",
+            "Your package does not have keywords data.",
+            "Your package does not have author data.",
+            "Your package does not have author_email data.",
+            "Your package does not have url data.",
+            "Your package does not have license data.",
+            "You are using Setuptools or Distribute but do not specify if this package is zip_safe or not. You should specify it, as it defaults to True, which you probably do not want.",
             "Setuptools and Distribute support running tests. By specifying a test suite, it's easy to find and run tests both for automated tools and humans.",
         ]))
 
@@ -170,18 +170,18 @@ class RatingsTest(unittest.TestCase):
             __name__, os.path.join('testdata', 'lacking'))
         data = projectdata.get_data(directory)
         rating = rate(data)
-        
+
         self.assertEqual(rating, (0, [
-            'The package had no description!', 
-            'The packages long_description is quite short.', 
-            'Your package does not have classifiers data.', 
-            'You should specify what Python versions you support.', 
-            'Your package does not have keywords data.', 
-            'Your package does not have author data.', 
-            'Your package does not have author_email data.', 
-            'Your package does not have url data.', 
-            'Your package does not have license data.', 
-            'You are using Setuptools or Distribute but do not specify if this package is zip_safe or not. You should specify it, as it defaults to True, which you probably do not want.',
+            "The package had no description!",
+            "The package's long_description is quite short.",
+            "Your package does not have classifiers data.",
+            "You should specify what Python versions you support.",
+            "Your package does not have keywords data.",
+            "Your package does not have author data.",
+            "Your package does not have author_email data.",
+            "Your package does not have url data.",
+            "Your package does not have license data.",
+            "You are using Setuptools or Distribute but do not specify if this package is zip_safe or not. You should specify it, as it defaults to True, which you probably do not want.",
             "Setuptools and Distribute support running tests. By specifying a test suite, it's easy to find and run tests both for automated tools and humans.",
         ]))
 
@@ -190,24 +190,24 @@ class RatingsTest(unittest.TestCase):
             __name__, os.path.join('testdata', 'custom_test'))
         data = projectdata.get_data(directory)
         rating = rate(data)
-        
+
         self.assertEqual(rating, (2, [
-            'The packages version number does not comply with PEP-386.', 
-            'The packages description should be longer than 10 characters.', 
-            'The packages long_description is quite short.', 
-            'Your package does not have classifiers data.', 
-            'You should specify what Python versions you support.', 
-            'Your package does not have keywords data.', 
-            'Your package does not have author data.', 
-            'Your package does not have author_email data.', 
-            'Your package does not have url data.', 
-            'Your package does not have license data.', 
-            'You are using Setuptools or Distribute but do not specify if this package is zip_safe or not. You should specify it, as it defaults to True, which you probably do not want.',            
+            "The package's version number does not comply with PEP-386.",
+            "The package's description should be longer than 10 characters.",
+            "The package's long_description is quite short.",
+            "Your package does not have classifiers data.",
+            "You should specify what Python versions you support.",
+            "Your package does not have keywords data.",
+            "Your package does not have author data.",
+            "Your package does not have author_email data.",
+            "Your package does not have url data.",
+            "Your package does not have license data.",
+            "You are using Setuptools or Distribute but do not specify if this package is zip_safe or not. You should specify it, as it defaults to True, which you probably do not want.",
         ]))
-        
+
 
 class PyPITest(unittest.TestCase):
-        
+
     def test_distribute(self):
         real_urlopen = urllib.urlopen
         real_server_proxy = xmlrpclib.ServerProxy
@@ -218,7 +218,7 @@ class PyPITest(unittest.TestCase):
             urllib.urlopen = urlopenstub
             data = pypidata.get_data('distribute')
             rating = rate(data)
-            
+
             self.assertEqual(rating, (9, [
                 'The classifiers should specify what minor versions of Python you support as well as what major version.',
                 'You should have three or more owners of the project on PyPI.'
@@ -237,25 +237,25 @@ class PyPITest(unittest.TestCase):
             urllib.urlopen = urlopenstub
             data = pypidata.get_data('complete')
             rating = rate(data)
-            
+
             self.assertEqual(rating, (10, []))
         finally:
             xmlrpclib.ServerProxy = real_server_proxy
             urllib.urlopen = real_urlopen
-            
+
 class ProjectDataTest(unittest.TestCase):
-    
+
     def test_complete(self):
         directory = resource_filename(
             __name__, os.path.join('testdata', 'complete'))
-        
+
         data = projectdata.get_data(directory)
         # The path is dynamic, and needs not be tested anyway:
         self.assertEqual(data, COMPLETE)
 
 
 class DistroDataTest(unittest.TestCase):
-    
+
     def test_complete(self):
         directory = resource_filename(
             __name__, os.path.join('testdata', 'distributions'))
