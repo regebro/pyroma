91b0e33e72e521d4868b4ca3bba863b7b9bec9b8
Max Tyulin <max@tyulin.ru>
2019-09-17 11:12:20 +0300

    setup.py may be absent

--- pyroma/projectdata.py
+++ pyroma/projectdata.py
@@ -143,30 +143,43 @@ def get_data(path):
     Returns data from a package directory.
     'path' should be an absolute path.
     """
+    metadata = {}
     # Run the imported setup to get the metadata.
     with FakeContext(path):
         with SetupMonkey() as sm:
-            try:
-                distro = run_setup('setup.py', stop_after='config')
+            if os.path.isfile('setup.py'):
+                try:
+                    distro = run_setup('setup.py', stop_after='config')
 
-                metadata = {'_setuptools': sm.used_setuptools}
+                    metadata = {'_setuptools': sm.used_setuptools}
 
-                for k, v in distro.metadata.__dict__.items():
-                    if k[0] == '_' or not v:
-                        continue
-                    if all(not x for x in v):
-                        continue
-                    metadata[k] = v
+                    for k, v in distro.metadata.__dict__.items():
+                        if k[0] == '_' or not v:
+                            continue
+                        if all(not x for x in v):
+                            continue
+                        metadata[k] = v
 
-                if sm.used_setuptools:
-                    for extras in ['cmdclass', 'zip_safe', 'test_suite']:
-                        v = getattr(distro, extras, None)
-                        if v is not None and v not in ([], {}):
-                            metadata[extras] = v
+                    if sm.used_setuptools:
+                        for extras in ['cmdclass', 'zip_safe', 'test_suite']:
+                            v = getattr(distro, extras, None)
+                            if v is not None and v not in ([], {}):
+                                metadata[extras] = v
+                except Exception as e:
+                    # Looks like setup.py is broken.
+                    logging.exception(e)
+                    metadata = {}
 
-            except ImportError as e:
-                # Either there is no setup py, or it's broken.
-                logging.exception(e)
-                metadata = {}
+            elif os.path.isfile('setup.cfg'):
+                try:
+                    from setuptools import config
+                    data = config.read_configuration('setup.cfg')
+                    metadata = data['metadata']
+                    metadata['_setuptools'] = True
+                except Exception as e:
+                    logging.exception(e)
+
+            else:
+                logging.exception("Neither setup.py nor setup.cfg was found")
 
     return metadata
