aaa338a78cc97e84ea161346bdda833b1a4d1701
Lennart <regebro@gmail.com>
2021-03-02 16:40:16 +0100

    The wheel shouldn't be universal, now when it's Python 3 only

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
