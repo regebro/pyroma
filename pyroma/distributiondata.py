61f83a71913c2d6ba94dec9f30de54da9477cd48
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-20 19:17:57 +0000

    Adding tarfile member sanitization to extractall()

--- pyroma/distributiondata.py
+++ pyroma/distributiondata.py
@@ -22,7 +22,26 @@ def get_data(path):
     try:
         if ext in (".bz2", ".tbz", "tb2", ".gz", ".tgz", ".tar"):
             with tarfile.open(name=path, mode="r:*") as tar_file:
-                tar_file.extractall(tempdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar_file, tempdir)
 
         elif ext in (".zip", ".egg"):
             with zipfile.ZipFile(path, mode="r") as zip_file:
